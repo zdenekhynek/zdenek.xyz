cf9834b861531275beda07b2800ce338e2cf8421
Zdenek Hynek <zdenek.hynek@gmail.com>
2020-06-03 15:36:48 +0100

    Gatsby starter

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zdenek.xyz
+
+Portfolio website
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/Layout";
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <div>
+        zdenek.xyz
+      </div>  
+    </Layout>
+  );
+};
+
+export default IndexPage;
